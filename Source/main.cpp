984dce795f671c3fcdb911b7eba3e774ec1068d8
elvisxiaozhi <elvisxiaozhi@gmail.com>
2018-02-02 21:21:40 +0800

    add srand(time(NULL));

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,8 +1,10 @@
 #include "interface.h"
 #include <QApplication>
+#include <cstdlib>
 
 int main(int argc, char *argv[])
 {
+    srand(time(NULL));
     QApplication a(argc, argv);
     Interface w;
     w.show();
